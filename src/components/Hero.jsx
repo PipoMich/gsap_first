7b8fd58cb49df07f1495ef527be8363214ea4522
PipoMich <188664623+PipoMich@users.noreply.github.com>
2025-07-24 09:40:30 +0100

    implement the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
-import React from "react";
+import React, { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -24,17 +28,36 @@ const Hero = () => {
       stagger: 0.06,
       delay: 1,
     });
-    gsap.timeline({
-      scrollTrigger:{
-        trigger:'#hero',
-        start:"top top",
-        end: "bottom top",
-        scrub:true,
-      }
-    })
-    .to('.right-leaf', {y:200},0)
-    .to('.left-leaf', {y:-200},0);
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
 
+    const startValue = isMobile ? "top50%" : "center60%";
+    const endValue = isMobile ? "120% top " : "bottom top";
+
+    const tl = gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "video",
+          start: startValue,
+          end: endValue,
+          scrub: true,
+          pin: true,
+        },
+      })
+      videoRef.current.onloadedmetadata = () =>{
+        tl.to(videoRef.current, {
+          currentTime:videoRef.current.duration,
+        })
+      }
   }, []);
 
   return (
@@ -72,6 +95,15 @@ const Hero = () => {
           </div>
         </div>
       </section>
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+      </div>
     </>
   );
 };
